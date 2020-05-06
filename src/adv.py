8936022e45f45c2d6a71aa118b86369586a0febe
Phong Yang <phongyang88@hotmail.com>
2020-05-06 17:58:38 -0500

    added room and player class. created a player that can navigate

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,18 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room('outside', "Outside Cave Entrance", "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room('foyer', "Foyer", "Dim light filters in from the south. Dusty passages run north and east."),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room('overlook', "Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room('narrow', "Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air."),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room('treasure', "Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
 }
 
 
@@ -38,6 +32,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player('outside')
 
 # Write a loop that:
 #
@@ -49,3 +44,51 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    print(f'Current room: {room[player.room].title}')
+    print(f'Room description: {room[player.room].description}')
+    user_input = input('Pick a direction to go: ')
+
+    if user_input == "q" or user_input == 'quit' or user_input == 'exit':
+        print("THANK YOU FOR PLAYING!")
+        break
+    elif user_input == "n" or user_input == 'north':
+        # if current room has .n_to then:
+        # player.room = current room.n_to
+        # else print that direction doesnt exist
+        if hasattr(room[player.room], 'n_to'):
+            print(f'You move north to: {room[player.room].n_to.title}')
+            player.room = room[player.room].n_to.name
+            print('\n')
+        else:
+            print(f"You cannot go north from here.")
+            print('\n')
+    elif user_input == "e" or user_input == 'east':
+        if hasattr(room[player.room], 'e_to'):
+            print(f'You move east to: {room[player.room].e_to.title}')
+            player.room = room[player.room].e_to.name
+            print('\n')
+        else:
+            print(f"You cannot go east from here.")
+            print('\n')
+    elif user_input == "s" or user_input == 'south':
+        if hasattr(room[player.room], 's_to'):
+            print(f'You move south to: {room[player.room].s_to.title}')
+            player.room = room[player.room].s_to.name
+            print('\n')
+        else:
+            print(f"You cannot go south from here.")
+            print('\n')
+    elif user_input == "w" or user_input == 'west':
+        if hasattr(room[player.room], 'w_to'):
+            print(f'You move west to: {room[player.room].w_to.title}')
+            player.room = room[player.room].w_to.name
+            print('\n')
+        else:
+            print(f"You cannot go west from here.")
+            print('\n')
+    else:
+        print(f"Invalid input. To quit, enter 'q'. To choose a direction to proceed, enter: n,e,s, or w")
+        print('\n')
+
--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, title, description):
+        self.name = name
+        self.title = title
+        self.description = description
+
+    # def __str__(self):
+    #     return 'f{self}'
